380b13a91c2448b3059644cba845e46ba76a83ea
purebeast46 <nhdnahid2017@gmail.com>
2022-05-03 18:58:30 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 let Question =  new Array;
 Question[0] = new Array("hello sage", "hi sage");
-Question[1] = new Array("who are you");
+Question[1] = new Array("who are you", "what should i call you");
 Question[2] = new Array("i am fine", "doing good");
 Question[3] = new Array("can you think");
+Question[4] =new Array("do you know anything");
+Question[5] =new Array("your favourite subject");
+Question[6] = new Array("your hobby");
+Question[7] = new Array("favourite food");
+Question[8] = new Array("do you dream");
+Question[9] = new Array("favourite game");
+Question[10] = new Array("where do you live");
 
 
 
 let Answer = new Array;
 Answer[0] = new Array("Hello, How are you doing?", "Hi there.", "Hey");
-Answer[1] = new Array("I am Sage", "I am your friend.", "You can call me sage.");
+Answer[1] = new Array("I am Sage", "I am your friend , Sage is my name.", "You can call me sage.");
 Answer[2] = new Array("That is great to know.");
 Answer[3] = new Array("Still I cannot think. But, I Nahid told me I would be able to think in the future.");
+Answer[4] = new Array("Ummm..... I suppose. But there may be many aspects in which I am still leaning.");
+Answer[5] = new Array("It Mathematics.");
+Answer[6] = new Array("Using Computer which has linux operating system");
+Answer[7] = new Array("It's mutton");
+Answer[8] = new Array("I dream about having a super computer");
+Answer[9] = new Array("I still cannot play outdoor games. I can only play video games. And I like Mobile Legends most.", "When I would be able to walk I will definitely play Basketball.");
+Answer[10] = new Array("I live inside a very dark place , that place is the darkest place of the universe. And it is your heart.");
